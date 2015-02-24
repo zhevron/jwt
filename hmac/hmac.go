907d87e8865e27994f968e916b6fa23abd6d07f1
Thomas Lokshall <thomas@lokshall.no>
2015-02-24 16:34:44 +0100

    Made the HMAC code slightly more DRY

--- hmac/hmac.go
+++ hmac/hmac.go
@@ -5,25 +5,26 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"crypto/sha512"
+	"hash"
 )
 
 // HS256 signs the given token with the given secret using HMAC SHA-256.
 func HS256(token string, secret []byte) []byte {
-	h := hmac.New(sha256.New, secret)
-	h.Write([]byte(token))
-	return h.Sum(nil)
+	return compute(token, hmac.New(sha256.New, secret))
 }
 
 // HS384 signs the given token with the given secret using HMAC SHA-384.
 func HS384(token string, secret []byte) []byte {
-	h := hmac.New(sha512.New384, secret)
-	h.Write([]byte(token))
-	return h.Sum(nil)
+	return compute(token, hmac.New(sha512.New384, secret))
 }
 
 // HS512 signs the given token with the given secret using HMAC SHA-512.
 func HS512(token string, secret []byte) []byte {
-	h := hmac.New(sha512.New, secret)
+	return compute(token, hmac.New(sha512.New, secret))
+}
+
+// compute calculates the hash for the token with the given algorithm.
+func compute(token string, h hash.Hash) []byte {
 	h.Write([]byte(token))
 	return h.Sum(nil)
 }
